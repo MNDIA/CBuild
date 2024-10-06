e65c7dc8dd3c809c7344770c56975a864c11c922
Nyan_yay <74826767+MNDIA@users.noreply.github.com>
2024-10-06 21:47:00 +0800

    Update gidchanger.c

--- src/gid-changer/gidchanger.c
+++ src/gid-changer/gidchanger.c
@@ -1,3 +1,4 @@
+// gidc
 #include <unistd.h>
 #include <sys/types.h>
 #include <stdio.h>
@@ -11,12 +12,18 @@ int main(int argc, char *argv[]) {
 
     pid_t pid = atoi(argv[1]);
     gid_t new_gid = 3005;
-
-    if (setresgid(new_gid, new_gid, new_gid) == -1) {
-        perror("setresgid");
+//通过syscall把指定的pid进程的gid改为new_gid
+    // 检查当前进程是否具有更改GID的权限
+    if (setgid(new_gid) == -1) {
+        perror("setgid");
         return 1;
     }
 
+    // 使用setpgid更改指定进程的GID
+    if (setpgid(pid, new_gid) == -1) {
+        perror("setpgid");
+        return 1;
+    }
     printf("GID changed successfully for PID %d\n", pid);
     return 0;
 }
